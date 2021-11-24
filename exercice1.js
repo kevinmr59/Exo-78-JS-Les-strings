0089df2a3ade704211986060af9764233d4e5597
kevinmr59 <kevinmura59610@gmail.com>
2021-11-24 18:25:59 +0100

    strings

--- exercice1.js
+++ exercice1.js
@@ -2,10 +2,24 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
+let one = monTexte.indexOf('i')
+console.log(one)
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let two =document.getElementById('position');
+two.innerHTML= 'one';
+
+let change = monTexte.toUpperCase();
+let changeTwo =document.getElementById('chaineMaj')
+changeTwo.innerHTML=change;
+for (let i=0; i<change.length; i++){
+    if (change.includes("A")){
+        alert("lettre trouver")
+    }
+}
+
 
 
 
